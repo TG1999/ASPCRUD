bc671288985ce773dd2b782bdad2e1c087ca0ba5
Tushar Goel <tushar.goel.dav@gmail.com>
2024-04-30 19:58:45 +0530

    Review
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a605faaf884c20b83300c11767fdbcaa895421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d93cecfd833d342fba180c75e7dcffa30828361")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
